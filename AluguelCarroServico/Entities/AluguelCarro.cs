90174cd1c2d7b57106f960fd80e914bb4620a26c
Fhzuccoli <fhzuccoli@gmail.com>
2019-10-20 22:41:17 -0300

    Iniciando criacao de entidades de classe para servicos

---
+++ AluguelCarroServico/Entities/AluguelCarro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AluguelCarroServico.Entities
+{
+    class AluguelCarro
+    {
+        public DateTime Comeco { get; set; }
+        public DateTime Fim { get; set; }
+        public Veiculo Veiculo { get; private set; }
+        public OrdemServico OrdemServico { get; set; }
+
+        public AluguelCarro(DateTime comeco, DateTime fim, Veiculo veiculo, OrdemServico ordemServico)
+        {
+            Comeco = comeco;
+            Fim = fim;
+            Veiculo = veiculo;
+            OrdemServico = ordemServico;
+        }
+    }
+}
---
+++ AluguelCarroServico/Entities/Veiculo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AluguelCarroServico.Entities
+{
+    class Veiculo
+    {
+    }
+}
